4c08b4ca3880cc1424ab26253de24fa5018a0245
pyh <pengyihao@baie.com.cn>
2019-04-27 12:08:55 +0800

    atxDemo

--- smokeTest/atxDemo.py
+++ smokeTest/atxDemo.py
@@ -1,11 +1,14 @@
-import atx
 from atx.ext.chromedriver import ChromeDriver
+import uiautomator2 as u2
 import time
 
-d = atx.connect()
-d.start_app('com.tencent.mm')
-driver = ChromeDriver(d).driver(package='com.tencent.mm', attach=True, activity='com.tencent.mm.ui.LauncherUI', process='com.tencent.mm:tools')
+# d = atx.connect()
+d = u2.connect('192.168.1.100')
+# d.start_app('com.tencent.mm')
+d.app_start('com.tencent.mm')
 time.sleep(9)
+driver = ChromeDriver(d).driver(package='com.tencent.mm', attach=True, activity='com.tencent.mm.ui.LauncherUI', process='com.tencent.mm:tools')
+
 print(1)
 print(driver.current_url)
 driver.find_element_by_id('globalSearch').click()
